c4ea1104fce5b242ec30bbdd1abb9d3b6d5db787
injaesong <injaesong0@gmail.com>
2025-10-31 02:02:13 +0900

    chore: add babel plugin for reanimated

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,7 @@ module.exports = function (api) {
     plugins: [
       ["babel-plugin-react-docgen-typescript", { exclude: "node_modules" }],
       "@babel/plugin-transform-class-static-block",
+      "@babel/plugin-proposal-export-namespace-from",
       "react-native-worklets/plugin",
     ],
   };
