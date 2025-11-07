128e592bf27b8050466c89a8ab4dfd9da74a101c
injaesong <injaesong0@gmail.com>
2025-11-07 12:49:17 +0900

    feat: migrate feed types and api callbacks

---
+++ app/(pages)/feed/feed.types.ts
@@ -0,0 +1,11 @@
+export interface DDuDuTimeType {
+  beginAt: string;
+  endAt: string;
+}
+
+export interface DDuDuTimeRangeType {
+  beginHour: number;
+  beginMin: number;
+  endHour: number;
+  endMin: number;
+}
---
+++ service/feed/feed.ts
@@ -0,0 +1,324 @@
+import { fetchApi } from "@/api";
+import { DDuDuTimeType } from "@/app/(pages)/feed/feed.types";
+import { FEED } from "@/constants/end-points";
+import {
+  RequestDDuDu,
+  RequestDDuDuChangeDate,
+  RequestPeriodGoals,
+} from "@/types/request/feed/feed";
+
+interface GetDailyListProps {
+  accessToken: string;
+  userId: number;
+  date: string;
+}
+
+export const getDailyList = async ({ accessToken, userId, date }: GetDailyListProps) => {
+  const selectedDate = `&date=${date}`;
+
+  const response = await fetchApi(`${FEED.DAILY_LIST}?userId=${userId}${selectedDate}`, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+export const getDailyTimeTable = async ({ accessToken, userId, date }: GetDailyListProps) => {
+  const response = await fetchApi(`${FEED.DAILY_TIMETABLE}?userId=${userId}&date=${date}`, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+interface PeriodGoalsProps {
+  accessToken: string;
+  type: "WEEK" | "MONTH";
+  date: string;
+}
+
+export const getGoals = async ({ accessToken, type, date }: PeriodGoalsProps) => {
+  const selectedDate = `&date=${date}`;
+
+  const response = await fetchApi(`${FEED.PERIOD_GOALS}?type=${type}${selectedDate}`, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+interface FetchCreateGoalsProps {
+  accessToken: string;
+  periodGoals: RequestPeriodGoals;
+}
+
+export const fetchCreateGoals = async ({ accessToken, periodGoals }: FetchCreateGoalsProps) => {
+  const response = await fetchApi(`${FEED.PERIOD_GOALS}`, {
+    method: "POST",
+    body: JSON.stringify(periodGoals),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+interface FetchEditGoalsProps {
+  accessToken: string;
+  contents: string;
+  periodGoalsId: number;
+}
+
+export const fetchEditGoals = async ({
+  accessToken,
+  contents,
+  periodGoalsId,
+}: FetchEditGoalsProps) => {
+  const response = await fetchApi(`${FEED.PERIOD_GOALS}/${periodGoalsId}`, {
+    method: "PUT",
+    body: JSON.stringify({ contents }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+export const getWeeklyDDuDus = async ({ accessToken, userId, date }: GetDailyListProps) => {
+  const selectedDate = `&date=${date}`;
+
+  const response = await fetchApi(`${FEED.WEEKLY_DDUDUS}?userId=${userId}${selectedDate}`, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+export const getMonthlyDDuDus = async ({ accessToken, userId, date }: GetDailyListProps) => {
+  const selectedDate = `&date=${date}`;
+
+  const response = await fetchApi(`${FEED.MONTHLY_DDUDUS}?userId=${userId}${selectedDate}`, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+export const getDDuDuDetail = async ({ accessToken, id }: FetchUpdateDDuDuProps) => {
+  const response = await fetchApi(`${FEED.DDUDU}/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+interface FetchCreateDDuDuProps {
+  accessToken: string;
+  requestDDuDu: RequestDDuDu;
+}
+
+export const fetchCreateDDuDu = async ({ accessToken, requestDDuDu }: FetchCreateDDuDuProps) => {
+  const response = await fetchApi(`${FEED.DDUDU}`, {
+    method: "POST",
+    body: JSON.stringify(requestDDuDu),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+interface FetchEditDDuDuProps {
+  accessToken: string;
+  id: number;
+  name: string;
+}
+
+export const fetchEditDDuDu = async ({ accessToken, id, name }: FetchEditDDuDuProps) => {
+  const response = await fetchApi(`${FEED.DDUDU}/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({ name }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+interface FetchUpdateDDuDuProps {
+  accessToken: string;
+  id: number;
+}
+
+export const fetchDeleteDDuDu = async ({ accessToken, id }: FetchUpdateDDuDuProps) => {
+  const response = await fetchApi(`${FEED.DDUDU}/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (response.status === 204) {
+    return response.status;
+  }
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+};
+
+export const fetchCompleteToggleDDuDu = async ({ accessToken, id }: FetchUpdateDDuDuProps) => {
+  const response = await fetchApi(`${FEED.DDUDU}/${id}/status`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (response.status !== 204) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+};
+
+interface fetchDDuDuDateProps {
+  accessToken: string;
+  id: number;
+  date: string;
+}
+
+export const fetchDDuDuChangeDate = async ({ accessToken, id, date }: fetchDDuDuDateProps) => {
+  const changedDate: RequestDDuDuChangeDate = { newDate: date };
+
+  const response = await fetchApi(`${FEED.DDUDU}/${id}/date`, {
+    method: "PUT",
+    body: JSON.stringify(changedDate),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (response.status !== 204) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.status;
+};
+
+export const fetchDDuDuRepeatDate = async ({ accessToken, id, date }: fetchDDuDuDateProps) => {
+  const response = await fetchApi(`${FEED.DDUDU}/${id}/repeat`, {
+    method: "POST",
+    body: JSON.stringify({ repeatOn: date }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+interface FetchDDuDUChangeTimeProps {
+  accessToken: string;
+  time: DDuDuTimeType;
+  id: number;
+}
+
+export const fetchDDuDuChangeTime = async ({
+  accessToken,
+  time,
+  id,
+}: FetchDDuDUChangeTimeProps) => {
+  const response = await fetchApi(`${FEED.DDUDU}/${id}/period`, {
+    method: "PUT",
+    body: JSON.stringify(time),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (response.status !== 204) {
+    throw new Error(`HHTP error! status: ${response.status}`);
+  }
+
+  return response.status;
+};
