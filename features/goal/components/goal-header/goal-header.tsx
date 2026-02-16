8fa3849d9fb9d5302298c530db8df38a1a7068db
injaesong <injaesong0@gmail.com>
2026-02-16 09:07:03 +0900

    feat: add goal page header

---
+++ features/goal/components/goal-header/goal-header.tsx
@@ -0,0 +1,53 @@
+import { Pressable, View } from "react-native";
+
+import { SpoqaText } from "@/components";
+import { ArrowLeftIcon, CreateIcon } from "@/icons";
+
+import { useRouter } from "expo-router";
+
+function GoalHeader() {
+  const router = useRouter();
+
+  const handlePressBack = () => {
+    router.push("/feed");
+  };
+
+  const handlePressCreate = () => {
+    router.push("/goal/create");
+  };
+
+  return (
+    <View className="px-[2.4rem] pb-[1.6rem] pt-[2.4rem]">
+      <View className="relative items-center justify-center">
+        <Pressable
+          onPress={handlePressBack}
+          className="absolute left-0 top-0 size-[2.4rem] items-start justify-center"
+          hitSlop={8}
+        >
+          <ArrowLeftIcon
+            size={16}
+            stroke="#FFFFFF"
+          />
+        </Pressable>
+        <SpoqaText
+          weight="bold"
+          className="text-size15 text-white"
+        >
+          Goals
+        </SpoqaText>
+        <Pressable
+          onPress={handlePressCreate}
+          className="absolute right-0 top-0 size-[2.4rem] items-end justify-center"
+          hitSlop={8}
+        >
+          <CreateIcon
+            size={18}
+            fill="#FFFFFF"
+          />
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+export default GoalHeader;
