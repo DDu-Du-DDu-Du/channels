40515f6534830badfb861f389e99054dd3f6c956
injaesong <injaesong0@gmail.com>
2026-02-19 01:31:15 +0900

    feat: enhance timestamp to adjust the height

--- components/timeline/components/time-stamp/time-stamp.tsx
+++ components/timeline/components/time-stamp/time-stamp.tsx
@@ -4,11 +4,16 @@ import { SpoqaText } from "@/components";
 
 export interface TimeStampProps {
   children: React.ReactNode;
+  fillParentHeight?: boolean;
 }
 
-function TimeStamp({ children }: TimeStampProps) {
+function TimeStamp({ children, fillParentHeight = false }: TimeStampProps) {
+  const wrapperHeightClassName = fillParentHeight ? "h-full" : "h-[5.7rem]";
+
   return (
-    <View className="flex w-[5rem] items-center justify-center pl-[1rem] h-[5.7rem]">
+    <View
+      className={`flex w-[5rem] items-center justify-center pl-[1rem] ${wrapperHeightClassName}`}
+    >
       <View className="flex h-[2.4rem] w-[4rem] items-center justify-center rounded-[0.5rem] bg-white_100">
         <SpoqaText className="text-size11">{children}</SpoqaText>
       </View>
